ba0e481f76e8a747b93b426a0c774ac3c654ef69
pantchayan <pantchayan@gmail.com>
2021-01-17 23:06:16 +0530

    average of 3 numbers

---
+++ Lab1/Q2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){ 
+    double a,b,c;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    double average = (a+b+c)/3;
+
+    cout << "Average is : " << average << endl;
+
+    return 0;
+}
