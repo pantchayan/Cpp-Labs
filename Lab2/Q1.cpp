5bbe1f049bb4e9f230ec3a28b9d8dfb488c31aec
pantchayan <pantchayan@gmail.com>
2021-01-19 20:12:04 +0530

    square and cube

---
+++ Lab2/Q1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int square = n*n;
+    int cube = n*n*n;
+
+    cout << square <<", " << cube << endl;
+
+    return 0;
+}
